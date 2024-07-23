41f21616e15057c42b9c1da8a35346b1ac602fc6
kigozi-14 <kigozie14@gmail.com>
2024-07-23 19:39:55 +0100

    we are going to try loading five posts at a time into our website

--- config.py
+++ config.py
@@ -5,10 +5,3 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql+psycopg2://default:HiOqm7sru9AX@ep-yellow-rice-a4tm4t92-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-
-
-# import os
-
-# class Config:
-#     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql+psycopg2://username:password@hostname/database_name'
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
