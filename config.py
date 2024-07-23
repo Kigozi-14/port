818a386824bb23241904551fb236ac941023d2bb
kigozi-14 <kigozie14@gmail.com>
2024-07-23 12:42:43 +0100

    struggling no time to explain

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', os.urandom(24))
-    SQLALCHEMY_DATABASE_URI = os.getenv('Database_URL', 'postgres_url_here')
+    SQLALCHEMY_DATABASE_URI = os.getenv('Database_URL', 'postgres://default:HiOqm7sru9AX@ep-yellow-rice-a4tm4t92-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
